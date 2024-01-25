4c859dce077060a20f50276587fa04376064089f
AyushT <invinciblefoxstar@gmail.com>
2024-01-25 23:56:39 +0530

    Deleted dummy data and removed importing of User model

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.contrib.auth.models import User
 
-posts=[
-    {
-        'author':"Ayush",
-        'title':"First Post",
-        'content':"My first post ever!!!",
-        'date_posted':"05-01-2024"
-    },
-    {
-        'author':"Luigi",
-        'title':"Second Post",
-        'content':"Mamma Miaaa",
-        'date_posted':"06-01-2024"
-    }
-]
 
 def home(request):
     return render(request, "blog/home.html",{
