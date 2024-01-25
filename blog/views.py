6cdd190ac04763e80bb9adb5995f4ac94925a38c
AyushT <invinciblefoxstar@gmail.com>
2024-01-25 23:50:36 +0530

    Fetching post data from database rather than dummmy data

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
+from .models import Post
+from django.contrib.auth.models import User
 
 posts=[
     {
         'author':"Ayush",
         'title':"First Post",
         'content':"My first post ever!!!",
-        'date':"05-01-2024"
+        'date_posted':"05-01-2024"
     },
     {
         'author':"Luigi",
         'title':"Second Post",
         'content':"Mamma Miaaa",
-        'date':"06-01-2024"
+        'date_posted':"06-01-2024"
     }
 ]
 
 def home(request):
     return render(request, "blog/home.html",{
-        'posts':posts
+        'posts':Post.objects.all()
     })
 
 def about(request):
