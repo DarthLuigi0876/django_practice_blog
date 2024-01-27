115fb564d673fa2dc734cd7e49e6e2e5854d290b
AyushT <invinciblefoxstar@gmail.com>
2024-01-27 19:32:31 +0530

    Adding more fields to user creation form

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+#Inherits from our usercreationform and used to add more fields like email, etc
+#Corey's Djano series part 6 second last part!
+
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        #nested name space for configs and keeps them in one place
+        #model that will be affected will be User model, and on form.save it will save it to User model
+        #the model is a user and gets validated thru it? ig?
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
