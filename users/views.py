f07d1318cbb54a397d5f49d5a7980c0ddec17ec5
AyushT <invinciblefoxstar@gmail.com>
2024-02-11 10:47:00 +0530

    Added login routes and logout routes, just made profile.html

--- users/views.py
+++ users/views.py
@@ -9,10 +9,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Your Account has been created!! You can log in now!")
+            messages.success(request, "Your Account has been created!! You can log in now!")
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {
         'form': form
     })
+    
+def profile(request):
+    return render(request, "users/profile.html")
