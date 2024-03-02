dcbe1a54eb3784a46580f6e0dd0b0e9ec5fc6201
AyushT <invinciblefoxstar@gmail.com>
2024-03-02 19:11:03 +0530

    Customizing profile

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -15,3 +15,19 @@ class UserRegisterForm(UserCreationForm):
         #the model is a user and gets validated thru it? ig?
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        #nested name space for configs and keeps them in one place
+        #model that will be affected will be User model, and on form.save it will save it to User model
+        #the model is a user and gets validated thru it? ig?
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
